12c02f12b76687ed03df09cfa9fa9d021143e9a6
aktm <aktm@yandex.ru>
2021-03-09 13:22:09 +0000

    create src dir with program.php

---
+++ src/program.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Gendiff;
+
+function run()
+{
+    $doc = <<<DOC
+    Generate diff
+
+    Usage:
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+
+    Options:
+      -h --help                     Show this screen
+      -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
+    DOC;
+
+    $args = \Docopt::handle($doc, ['version' => 'Generate diff v.0.1']);
+}
